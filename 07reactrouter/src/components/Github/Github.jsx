16cd35ae8cc4d4349a583ebad4f799ee8f9e742d
Abhilash Singh <firehawk039@gmail.com>
2026-02-11 03:30:08 +0530

    gituibh

--- 07reactrouter/src/components/Github/Github.jsx
+++ 07reactrouter/src/components/Github/Github.jsx
@@ -1,18 +1,29 @@
-import { use, useEffect, useState } from "react"
 
-export default function Github() {
-    const [data, setData] = useState()
-    useEffect(() => {
-        fetch("https://api.github.com/users/firehawk039")
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                setData(data)
-            })
-    }, []) 
-    return (
-        <div className="text-center m-4 bg-grey-600 text-white -4 text-3xl">Github followers: {data?.followers}
-        <img src={data?.avatar_url} alt="Git Picture" width={300} className="rounded-full w-32 h-32 mx-auto mt-4"/>
-        </div>
-    )
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+    // const [data, setData] = useState([])
+    // useEffect(() => {
+    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  .then(response => response.json())
+    //  .then(data => {
+    //     console.log(data);
+    //     setData(data)
+    //  })
+    // }, [])
+    
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>Github followers: {data.followers}
+    <img src={data.avatar_url} alt="Git picture" width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
 }
