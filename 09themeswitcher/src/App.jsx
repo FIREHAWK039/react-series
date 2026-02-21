817d51d6371ce3e8b726d4e03c7254530d09435a
Abhilash Singh <firehawk039@gmail.com>
2026-02-21 21:18:36 +0530

    theme provider

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -4,12 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode] = useState("light");
 
   return (
-    <>
-      <h1 className='p-4 bg-pink-600 text-3xl'>react</h1>
-    </>
+    <ThemeProvider value={{themeMode, darktheme, lighttheme,}}>
+
+
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          {/* Themebtn */}
+
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          {/* Card */}
+
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
   )
 }
 
